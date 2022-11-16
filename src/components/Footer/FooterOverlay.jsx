7f5480fcac25bb949ea31270134ed6236f651430
Yash Janyani <yash.janyani2000@gmail.com>
2022-11-16 13:49:53 +0530

    added map

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -4,6 +4,7 @@ import "./FooterOverlay.css";
 
 const FooterOverlay = () => (
   <div className="app__footerOverlay">
+  {/* <iframe src="//www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3557.500946728754!2d75.82339781488334!3d26.919329083124687!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db6b03af9295f%3A0x5bb4333133c6968!2sAlbert%20Hall%20Museum!5e0!3m2!1sen!2sin!4v1668582805854!5m2!1sen!2sin" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
     <div className="app__footerOverlay-black" />
     <div className="app__footerOverlay-img app__bg" />
   </div>
--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -35,7 +35,19 @@ const Newsletter = () => (
     </div>
 
 
+    <div className="app__newsletter-heading">
+      <h1 className="headtext__cormorant">Locate us:</h1>
+      <iframe
+   src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3557.500946728754!2d75.82339781488334!3d26.919329083124687!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db6b03af9295f%3A0x5bb4333133c6968!2sAlbert%20Hall%20Museum!5e0!3m2!1sen!2sin!4v1668582805854!5m2!1sen!2sin'
+
+   style={{ border: 0 }}                        //<-------
+   allowFullscreen=''                           //<-------
+   loading='lazy'
+   referrerPolicy='no-referrer-when-downgrade'  //<-------
+></iframe>    </div>
   </div>
 );
 
 export default Newsletter;
+
+{/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3557.500946728754!2d75.82339781488334!3d26.919329083124687!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db6b03af9295f%3A0x5bb4333133c6968!2sAlbert%20Hall%20Museum!5e0!3m2!1sen!2sin!4v1668582805854!5m2!1sen!2sin" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
